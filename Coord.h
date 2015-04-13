2a5b24547b25b644a7c4e2ed4494d1dc1005132b
gnorkx <gnorkx@users.noreply.github.com>
2015-04-13 18:32:24 +0200

    Add cros() & SinTheta()

--- Coord.h
+++ Coord.h
@@ -27,9 +27,11 @@ class Coord{
     void swap(Coord&);
     float abs() const {return sqrt(x*x + y*y);} ;
     float absqr() const {return x*x + y*y;} ;
-    float CosTheta( const Coord v ) const {return (x*v.x + y*v.y)/sqrt((x*x + y*y)*(v.x*v.x+v.y*v.y));};
-    float Theta( const Coord v ) const {return acos(CosTheta(v))/M_PI*180.;};
+    float CosTheta( const Coord &v ) const {return (x*v.x + y*v.y)/sqrt((absqr()*v.absqr());};
+    float SinTheta( const Coord &v ) const {return cross(v)/sqrt(absqr()*v.absqr());};
+    float Theta( const Coord &v ) const {return acos(CosTheta(v))/M_PI*180.;};
     Coord unit() const {return Coord(x/abs(),y/abs());} ;
+    Coord cross(const Coord &v) const {return Coord(y*v.x-x*v.y, x*v.y-y*v.x);};
     void rotate(float);
     Coord perp() const;
 
